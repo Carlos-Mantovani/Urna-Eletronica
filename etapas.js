a8431c54a317c4b45080994fce4d987b74173d69
CarlosStrider120 <sherifinho.augusto@gmail.com>
2021-09-04 02:39:43 -0300

    Criando arquivo etapas, adicionando candidatos e informações

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Samurai Jack',
+                partido: 'ABC',
+                fotos: [
+                    {url: '38111.webp', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Puro-Osso',
+                partido: 'DEFG',
+                fotos: [
+                    {url: '77222.webp', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+        {
+            numero: 99,
+            nome: 'Gumball',
+            partido: 'ABC',
+            vice: 'Darwin',
+            fotos: [
+                {url: '99.webp', legenda: 'Prefeito'},
+                {url: '99_2.webp', legenda: 'Vice-Prefeito', small: true}
+            ]
+        },
+        {
+            numero: 84,
+            nome: 'Bob-Esponja',
+            partido: 'ABC',
+            vice: 'Patrick',
+            fotos: [
+                {url: '84.webp', legenda: 'Prefeito'},
+                {url: '84_2.webp', legenda: 'Vice-Prefeito', small: true}
+            ]
+        },
+        ]
+    }
+];
